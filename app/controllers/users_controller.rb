075638818439f02d389315ebc220baabb1e40ce0
Rails Issues <zmerzed@gmail.com>
2016-03-07 14:34:46 +0000

    create users show action and gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,8 +31,10 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		
 	end
+	
+	
+	
 	private
 		def user_params
 			params.require(:user).permit(:username, :email, :password)
